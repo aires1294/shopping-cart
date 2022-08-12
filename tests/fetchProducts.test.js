b07e9e03c74c9b0892be91cefc6ac4e67780d751
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-12 18:03:36 -0300

    item 1 feito

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('deve retornar se a função fetch foi chamada', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  test('Chamar a função fetchProducts com computador como argumento', async () => {
+    const fetchComputer = await fetchProducts('computador');
+    expect(fetchComputer).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  test('', async () => {
+    const fetchComputer = await fetchProducts('computador');
+    expect(fetchComputer).toEqual(computadorSearch);    
+  });
+  test('', async () => {
+    const fetchComputer = await fetchProducts();
+    expect(fetchComputer).toEqual(new Error('You must provide an url'));
+  });
+ 
 });
